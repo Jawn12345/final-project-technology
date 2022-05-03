b4f85a4078b30b55051525403fcc0d251e797174
Jawn12345 <lambertwu5@gmail.com>
2022-05-03 06:51:34 -0400

    added content page

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+anime({
+  targets: '.circle',
+  translateX: 250,
+  delay: anime.stagger(200, {start: 1000}),
+  direction: "reverse",
+  loop: true
+});
+
+// anime({
+//   targets: '.circle',
+//   keyframes: [
+//     {translateY: -40},
+//     {translateX: 250},
+//     {translateY: 40},
+//     {translateX: 0},
+//     {translateY: 0}
+//   ],
+//   duration: 4000,
+//   easing: 'easeOutElastic(1, .8)',
+//   loop: true
+// });
